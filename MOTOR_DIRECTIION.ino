31aec11b1505b21850d55c56bbcb09daa031477b
NidhinJose <104612066+NidhinJose@users.noreply.github.com>
2025-01-07 10:09:20 +0530

    Add files via upload

---
+++ MOTOR_DIRECTIION.ino
@@ -0,0 +1,99 @@
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+
+
+#define WLAN_SSID       "Realme"
+#define WLAN_PASS       "012345688"
+
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "akshaypsmec"
+#define AIO_KEY         "bb1f21fe02c74595b5572e7c728caa36"
+
+WiFiClient client;
+
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+Adafruit_MQTT_Subscribe o = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/analog");
+
+void MQTT_connect();
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println(F("Adafruit MQTT demo"));
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);//
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+pinMode(D8,OUTPUT);
+pinMode(D6,OUTPUT);
+mqtt.subscribe(&o);
+}
+
+uint32_t x=0;
+
+void loop() {
+String a;  
+  MQTT_connect();
+Adafruit_MQTT_Subscribe *subscription;
+  
+while ((subscription = mqtt.readSubscription(5000))) {
+    if (subscription == &o) {
+      Serial.print("Got: ");
+ a=(char *)o.lastread;
+      Serial.println(strcmp((char *)o.lastread,"1"));    }
+  }
+  if(strcmp((char *)o.lastread,"A"))
+  {
+  digitalWrite(D6,1);
+  digitalWrite(D8,0);
+  }
+  else
+  {
+    digitalWrite(D6,0);
+    digitalWrite(D8,1);
+    }
+  
+  delay(2000);
+ 
+
+}
+
+
+void MQTT_connect() {
+  int8_t ret;
+
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
